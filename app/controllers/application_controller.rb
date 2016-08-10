7adf8a72ae0324e33b61c6c126a500436aefbab5
ozPop <ozziepopovas@gmail.com>
2016-08-09 20:03:32 -0400

    refactor views, application controller
    move account.erb file
    update application_controller to reflect views changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,19 +7,36 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
-    erb :index
+    erb :'/sessions/index'
   end
 
   post '/login' do
-
+    @user = User.find_by(username: params[:username])
+    if @user
+      session[:user_id] = @user.id
+      redirect '/account'
+    else
+      erb :'/sessions/error'
+    end
   end
 
   get '/account' do
-
+      binding.pry
+    if Helpers.is_logged_in?(session)
+      @user = Helpers.current_user(session)
+      erb :account
+    else
+      erb :'/sessions/error'
+    end
   end
 
   get '/logout' do
+    session.clear
+    redirect '/'
+  end
 
+  get '/sessions/error' do
+    erb :'/sessions/error'
   end
 
 
