73881d95a9ac9d961ababbb80228a2942dcd34a7
Flatiron School <github email address>
2016-08-22 15:28:08 -0400

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,21 @@
 class Helpers
+
+  def self.current_user(session)
+    #argument is session hash
+    #using the user_id from the session id
+    # @user_id =
+    @user = User.find_by_id(session[:user_id])
+  end
+
+
+  def self.is_logged_in?(session)
+
+    !! session[:user_id]
+    # if session[:user_id] == @user_id
+    #   true
+    # else
+    #   false
+    # end
+  end
+
 end
