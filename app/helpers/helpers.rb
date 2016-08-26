1363b20806cc85d821c4f4ae4ed109b43c5af6b1
Dmitriy Khait <khaitdmitriy@gmail.com>
2016-08-26 12:17:18 -0400

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,14 @@
-class Helpers
+# require 'sinatra/base'
+
+class Helpers < ActiveRecord::Base
+  def self.current_user(session_info)
+    @user = User.find_by_id(session_info[:user_id])
+    # binding.pry
+  end
+
+
+  def self.is_logged_in?(session_info)
+    !!session_info[:user_id]
+  end
+
 end
