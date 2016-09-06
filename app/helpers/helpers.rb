549e2d292b8a70a7a6da494a473f002273f36507
sami4thewin <samiomarmohammed@gmail.com>
2016-09-06 14:55:28 -0400

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,16 @@
 class Helpers
+
+  def self.current_user(session)
+    @user = User.find(session[:user_id])
+    # binding.pry
+    @user
+  end
+
+  def self.is_logged_in?(session)
+    !!session[:user_id]
+    # binding.pry
+    # !!Helpers.current_user(session)
+  end
+
+
 end
