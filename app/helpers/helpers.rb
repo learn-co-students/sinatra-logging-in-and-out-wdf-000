b1aaa4f2e901952094a3cb14d9192f910cd38814
Esmery Corniel <esmerycornielle@gmail.com>
2016-09-13 17:32:30 -0400

    Done

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,12 @@
 class Helpers
+
+  def self.current_user(sessions_hash)
+    User.find_by(id: sessions_hash[:user_id])
+  end
+
+  def self.is_logged_in?(sessions_hash)
+    !!sessions_hash[:user_id]
+  end
+
+
 end
